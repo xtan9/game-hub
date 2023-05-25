fe2d45d038861b6c98e0ad2db574924cd6a5d733
Xingdi Tan <steventanxd@gmail.com>
2023-05-25 14:09:39 -0700

    Display critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,16 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color = score > 80 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <Badge fontSize="14px" paddingX={2} borderRadius={4} colorScheme={color}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
