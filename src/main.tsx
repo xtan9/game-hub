8b0d22ff730f6cad92383b3e141bd9d0cff62905
Xingdi Tan <steventanxd@gmail.com>
2023-06-12 15:12:26 -0700

    Customize the theme to get darker grays

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.tsx";
-import theme from "./them.ts";
+import theme from "./theme.ts";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
